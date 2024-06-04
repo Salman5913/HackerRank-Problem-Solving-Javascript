b31989979ac2bc0052d32310fc3570741da58f97
Salman Abdul Rehman <127036525+Salman5913@users.noreply.github.com>
2024-06-04 11:22:46 +0500

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,13 @@
+
+// Complete the function solveMeFirst to compute the sum of two integers-(HackerRank)
+let solveMeFirst = (a, b) => {
+    return a+b;
+}
+// Simple Array Sum Solution-(HackerRank)
+let simpleArraySum = (ar) => {
+    let sum = 0;
+    for(let element of ar){
+        sum += element;
+    }
+    return sum;
+}
