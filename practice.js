1b11611fd61448ceff96f84e35ebb465d9ab84f9
Apptycoons <apptycoons@Apptycoonss-Mac-mini.local>
2024-06-05 12:11:48 +0500

    solution/A very Big Sum ,  Diagonal difference

--- practice.js
+++ practice.js
@@ -22,3 +22,23 @@ let compareTriplets = (a, b) => {
     }
     return result;
 }
+//In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+function aVeryBigSum(ar) {
+    let sum = 0;
+    for(let element of ar){
+        sum += element
+    }
+    return sum;
+}
+// Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+let diagonalDifference = (arr) => {
+    let leftDiagonalSum = 0;
+    let rightDiagonalSum = 0;
+    let len = arr.length;
+    for(let i = 0; i < len; i++){
+        leftDiagonalSum += arr[i][i];
+        rightDiagonalSum += arr[i][len-(i+1)];
+    }
+    let difference = Math.abs(leftDiagonalSum - rightDiagonalSum);
+    return difference;
+}
