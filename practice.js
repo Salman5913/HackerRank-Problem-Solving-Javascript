cafad7a282b35e46cdedadf843f3757676707ec7
Apptycoons <apptycoons@Apptycoonss-Mac-mini.local>
2024-06-06 11:54:08 +0500

    Plus Minus and Stair Case-Solution (HackerRank)

--- practice.js
+++ practice.js
@@ -42,3 +42,42 @@ let diagonalDifference = (arr) => {
     let difference = Math.abs(leftDiagonalSum - rightDiagonalSum);
     return difference;
 }
+//Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.soution-(HackerRank)
+let plusMinus = (arr) => {
+    let positiveNumbers = 0;
+    let negativeNumbers = 0;
+    let zeros = 0;
+    let len = arr.length;
+    for(let key in arr){
+        if(arr[key] > 0){
+            positiveNumbers++;
+        }
+        else if(arr[key] < 0){
+            negativeNumbers++;
+        }
+        else{
+            zeros++;
+        }
+    }
+    let positiveRatio = (positiveNumbers/len).toFixed(6);
+    let negativeRatio = (negativeNumbers/len).toFixed(6);
+    let zeroRatio = (zeros/len).toFixed(6);
+    console.log(positiveRatio);
+    console.log(negativeRatio);
+    console.log(zeroRatio);
+}
+
+//Staircase challenge solution-(HackerRank)
+let staircase = (n) => {
+    for(let i = 1; i <= n ; i++){
+        let line = '';
+        for(let j = 1; j <= n; j++){
+            if(j <= n-i){
+                line += ' '; 
+            }else{
+                line += '#';                 
+            }
+        }
+        console.log(line);
+    }
+}
